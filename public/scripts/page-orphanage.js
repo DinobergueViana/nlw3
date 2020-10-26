fa9133ae82e559e032c12c82c6544702fef07cf8
DinobergueViana <dinobergueviana@gmail.com>
2020-10-26 16:40:45 -0300

    finalizando a pagina orphanage

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -24,3 +24,28 @@ const icon = L.icon({
 L
     .marker([-27.2205683,-49.6451598], { icon })
     .addTo(map)
+
+/* galeria de imagens */
+
+function selectImage(event){
+    const button = event.currentTarget;
+    
+    // remove todas as classes .active
+    const buttons = document.querySelectorAll(".images button");
+
+    buttons.forEach(removeActiveClass);
+
+    function removeActiveClass(button) {
+        button.classList.remove("active"); 
+    }
+
+    // seleciona a imagem clicada
+    const image = button.children[0];
+    const imageContainer = document.querySelector('.orphanage-details > img');
+
+    // atualiza o container com a imagem ativa
+    imageContainer.src = image.src;
+
+    // adiciona a classe .active para o botão clicado
+    button.classList.add('active');
+}
